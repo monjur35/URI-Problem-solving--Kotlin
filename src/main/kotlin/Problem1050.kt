0af37dc2ef0a7876758b76f985f8c57dca2ed297
monjur35 <monjur35-1019@diu.edu.bd>
2021-11-30 16:50:37 +0600

    Problem 1050 done

---
+++ src/main/kotlin/Problem1050.kt
@@ -0,0 +1,33 @@
+fun main() {
+
+    val s= readLine()!!
+    val ddd=s.toInt()
+
+    if (ddd==61){
+        println("Brasilia")
+    }
+    else if (ddd==71){
+        println("Salvador")
+    }
+    else if (ddd==11){
+        println("Sao Paulo")
+    }
+    else if (ddd==21){
+        println("Rio de Janeiro")
+    }
+    else if (ddd==32){
+        println("Juiz de Fora")
+    }
+    else if (ddd==19){
+        println("Campinas")
+    }
+    else if (ddd==27){
+        println("Vitoria")
+    }
+    else if (ddd==31){
+        println("Belo Horizonte")
+    }
+    else {
+        println("DDD nao cadastrado")
+    }
+}
