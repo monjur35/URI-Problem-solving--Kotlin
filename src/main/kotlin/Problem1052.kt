52ad45f8f6ace19f25785bb1fa34d1a9a25a2cb5
monjur35 <monjur35-1019@diu.edu.bd>
2021-11-30 18:15:02 +0600

    Problem 1052 done

---
+++ src/main/kotlin/Problem1052.kt
@@ -0,0 +1,46 @@
+fun main() {
+    val s = readLine()!!
+    val month = s.toInt()
+
+    if (month == 1) {
+        println("January")
+    }
+    else if (month == 2) {
+        println("February")
+    }
+    else if (month == 3) {
+        println("Merch")
+    }
+    else if (month == 4) {
+        println("April")
+    }
+    else if (month == 5) {
+        println("May")
+    }
+    else if (month == 6) {
+        println("June")
+    }
+    else if (month == 7) {
+        println("July")
+    }
+    else if (month == 8) {
+        println("August")
+    }
+    else if (month==9){
+        println("October")
+    }
+    else if (month==10){
+        println("September")
+    } else if (month==11){
+        println("November")
+    }
+    else if (month==2){
+        println("December")
+    }
+
+    else {
+
+    }
+
+
+}
