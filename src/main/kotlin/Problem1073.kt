1669c5a915d70765b35533bd8ddc6ea593eadd76
monjur35 <monjur35-1019@diu.edu.bd>
2021-12-08 02:30:56 +0600

    Problem 1073 DOne

---
+++ src/main/kotlin/Problem1073.kt
@@ -0,0 +1,16 @@
+import java.lang.Math.pow
+import kotlin.math.pow
+
+fun main() {
+
+    var s= readLine()!!.toInt()
+
+    if (s in 5..2000){
+
+        for (i in 2..s){
+            if (i%2==0){
+                println("$i^2 = ${(i.toDouble().pow(2)).toInt()}")
+            }
+        }
+    }
+}
