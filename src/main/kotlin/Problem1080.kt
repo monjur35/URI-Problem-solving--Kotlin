c9b5aae74b6a3f0222eb86c578e34f9602615e1a
monjur35 <monjur35-1019@diu.edu.bd>
2021-12-08 19:29:39 +0600

    Problem 1080 DOne

---
+++ src/main/kotlin/Problem1080.kt
@@ -0,0 +1,16 @@
+fun main() {
+    var s0=-1
+    var inputPosition=1
+
+    for (i in 1 until 101){
+        var s= readLine()!!.toInt()
+
+        if (s>s0){
+            s0=s
+            inputPosition=i
+        }
+    }
+
+    println(s0)
+    println(inputPosition)
+}
