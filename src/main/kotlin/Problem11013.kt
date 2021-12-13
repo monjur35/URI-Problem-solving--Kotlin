9d32881a4adaa468995bb1a76f26430eded0afe4
monjur35 <monjur35-1019@diu.edu.bd>
2021-12-14 00:05:32 +0600

    Problem 1113  done

--- src/main/kotlin/Problem11013.kt
+++ src/main/kotlin/Problem11013.kt
@@ -1,9 +1,9 @@
 fun main() {
     while (true){
 
-        var s= readLine()!!.split(" ")
-        var a=s[0].toInt()
-        var b=s[1].toInt()
+        val s= readLine()!!.split(" ")
+        val a=s[0].toInt()
+        val b=s[1].toInt()
 
         if (a==b){
             break
