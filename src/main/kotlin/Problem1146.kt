a2a6dd56315cdc71d305c9110ef5796d6e72849d
monjur35 <monjur35-1019@diu.edu.bd>
2021-12-20 18:34:49 +0600

    Problem 1146 Done

--- src/main/kotlin/Problem1146.kt
+++ src/main/kotlin/Problem1146.kt
@@ -1,3 +1,22 @@
 fun main() {
 
+    while (true){
+        var x= readLine()!!.toInt()
+
+        if (x==0){
+            break
+        }
+        else {
+            for (i in 1..x){
+
+                if (i!=x){
+                    print("$i ")
+                }
+                else{
+                    print(i)
+                }
+            }
+            println()
+        }
+    }
 }
