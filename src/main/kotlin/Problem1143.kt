c3f99f3f77c7e2fd6444802cc762f3260387d1de
monjur35 <monjur35-1019@diu.edu.bd>
2021-12-20 16:52:04 +0600

    Problem 1143  trying

--- src/main/kotlin/Problem1143.kt
+++ src/main/kotlin/Problem1143.kt
@@ -2,10 +2,20 @@ fun main() {
 
     var n= readLine()!!.toInt()
 
+
     for (i in 1..n){
-        for (j in 1..3){
-            print("${i*j} ")
+        print("$i ")
+        for (j in 1..2){
+
+            if (j==1){
+                print("${i*i} ")
+            }
+            else if (j==2){
+                print("${i*i*i}")
+                println()
+            }
+
         }
-        println()
+
     }
 }
