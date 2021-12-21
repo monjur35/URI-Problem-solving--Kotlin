8f3751b3628f1210c9d04c6fadc376fecb1eb082
monjur35 <monjur35-1019@diu.edu.bd>
2021-12-21 23:58:31 +0600

    Problem 1151 Done.

--- src/main/kotlin/Problem1151.kt
+++ src/main/kotlin/Problem1151.kt
@@ -1,16 +1,39 @@
 fun main() {
     var n= readLine()!!.toInt()
-    var x=0
+    var n1 =0
+    var n2 =1
+    var t :Int
 
     if (n<46){
-        for (i in 0..4){
-            x+=i
-            if (i==4){
-                print(x)
+        for (i in 1..n){
+
+
+
+            if (i==1){
+                n1=0
+                print("$n1")
+
+            }
+            else if (i==2){
+                n2=1
+                print("$n2")
             }
             else{
-                print("$x ")
+                t=n1+n2
+                print(t)
+                n1=n2
+                n2=t
             }
+
+            if (i==n){
+                println()
+            }
+            else{
+                print(" ")
+            }
+
+
+
         }
     }
 }
