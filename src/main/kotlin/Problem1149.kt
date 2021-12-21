a587e286bca59ae2d0543e5e367cb30936ca1698
monjur35 <monjur35-1019@diu.edu.bd>
2021-12-21 14:26:10 +0600

    Problem 1149 runtime error

--- src/main/kotlin/Problem1149.kt
+++ src/main/kotlin/Problem1149.kt
@@ -1,2 +1,21 @@
-class Problem1149 {
+fun main() {
+
+    val s = readLine()!!.split(" ")
+    val a = s[0].toInt()
+    var b = s[1].toInt()
+    var j = 0
+
+
+
+
+
+
+    while (b <= 0) {
+        b = readLine()!!.toInt()
+    }
+    for (i in 0 until b) {
+        j += a + i
+    }
+
+    print(j)
 }
