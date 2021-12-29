2e90a3ae39c82e0b7e8ba52abe2ba9f1f5a25155
monjur35 <monjur35-1019@diu.edu.bd>
2021-12-30 01:17:09 +0600

    Problem 1156 done.

--- src/main/kotlin/Problem1156.kt
+++ src/main/kotlin/Problem1156.kt
@@ -1,3 +1,13 @@
 fun main() {
+    var a = 1.0
+    var b = 1.0
+    var s=0.0
+    while (a <= 39) {
+        var c=a/b
+        s+=c
+        b*=2
+        a+=2
+    }
+    println("%.2f".format(s))
 
 }
