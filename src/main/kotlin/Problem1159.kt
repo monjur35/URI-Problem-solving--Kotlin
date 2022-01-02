a21bbd78bc3fbcff684995f81b7e3acd891dc80b
monjur35 <monjur35-1019@diu.edu.bd>
2022-01-03 00:11:24 +0600

    Problem 1159 done.

--- src/main/kotlin/Problem1159.kt
+++ src/main/kotlin/Problem1159.kt
@@ -1,3 +1,28 @@
 fun main() {
 
+
+
+    while (true){
+        var sum=0
+        var x= readLine()!!.toInt()
+        if (x==0){
+           break
+        }
+        else if (x%2==0){
+
+        }
+        else{
+            x += 1
+        }
+
+        for (i in 1..5){
+            sum += x
+            x += 2
+        }
+        println(sum)
+
+    }
+
+
+
 }
