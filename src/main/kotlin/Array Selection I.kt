378988d6eb92075877f14ceb6ada47b224c8aab1
monjur35 <monjur35-1019@diu.edu.bd>
2022-01-05 20:07:56 +0600

    Problem  Done

--- src/main/kotlin/Array Selection I.kt
+++ src/main/kotlin/Array Selection I.kt
@@ -1,3 +1,16 @@
 fun main() {
-    var array= charArrayOf()
+    var array= FloatArray(100)
+
+    for (i in 0..10){
+        var x= readLine()!!.toFloat()
+        array[i] = x
+    }
+
+    for (j in 0..10){
+        if (array[j]<=10){
+            println("A[$j] = ${"%.1f".format(array[j])}")
+        }
+    }
 }
+
+
