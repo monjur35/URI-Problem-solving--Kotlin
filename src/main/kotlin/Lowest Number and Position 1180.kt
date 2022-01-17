1e491c50cbce5be708e21819844d8e2b1ab851c4
monjur35 <monjur35-1019@diu.edu.bd>
2022-01-18 00:58:04 +0600

    Lowest Number and Position 1180 Trying

---
+++ src/main/kotlin/Lowest Number and Position 1180.kt
@@ -0,0 +1,28 @@
+fun main() {
+    var n= readLine()!!.toInt()
+    var array=Array<Int>(n+1){0}
+    var index =0
+    var value =0
+
+    var x= readLine()!!.split(" ")
+    for (i in 0 until n){
+
+        array[i]=x[i].toInt()
+        println(array[i])
+
+    }
+    for (j in 0 until n ){
+        if(array[j]<array[j+1]){
+            index=j
+            value=array[j]
+        }
+        else{
+            index=j+1
+            value=array[j+1]
+        }
+    }
+
+    println("Menor valor: $value")
+    println("Posicao: $index")
+
+}
